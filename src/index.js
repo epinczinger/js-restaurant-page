7148edf38f709b5f49e0d314bf4ea34d52b660da
EstebanP <epinczinger@gmail.com>
2020-11-12 13:00:36 -0300

    fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import _ from "lodash";
+import _ from 'lodash';
 
 function component() {
-  const element = document.createElement("div");
+  const element = document.createElement('div');
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
