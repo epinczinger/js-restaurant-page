41b8c8ecf530867ff751e07eb2f147737b04fe3f
EstebanP <epinczinger@gmail.com>
2020-11-15 10:53:28 -0300

    add contact.js and render on index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-
-import renderHome from './content/home'
+import renderContact from './content/contact';
+import renderHome from './content/home';
 import renderNavbar from "./content/navbar";
-import renderMenu from './content/menu'
+import renderMenu from './content/menu';
 
 renderNavbar();
 renderHome();
 renderMenu();
-
+renderContact();
 
 
 
